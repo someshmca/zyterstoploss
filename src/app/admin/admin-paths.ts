03b6b2fba69236ff8aad12946775d0ddfe7e32b0
Nukala Somesh <nukalas@infinite.com>
2021-07-08 18:37:14 +0530

    qa box localhost 92 change in the admin paths file

--- src/app/admin/admin-paths.ts
+++ src/app/admin/admin-paths.ts
@@ -95,96 +95,193 @@
 
 
 
-export const Paths = {
-  loginPath : "http://10.48.30.158:92/api/Login/UserLogin/",
-  reportPath : "http://10.48.30.158:92/api/Claim/GetClaimReport",
-  claimPath: "http://10.48.30.158:92/api/Claim/GetClaim",
+// export const Paths = {
+//   loginPath : "http://10.48.30.158:92/api/Login/UserLogin/",
+//   reportPath : "http://10.48.30.158:92/api/Claim/GetClaimReport",
+//   claimPath: "http://10.48.30.158:92/api/Claim/GetClaim",
   
   
-  attributes: "http://10.48.30.158:92/api/Attribute/GetAllAttributeID",
-  attribute: "http://10.48.30.158:92/api/Attribute/GetAttributeDetails?attributeId=",
-  attributeAdd: "http://10.48.30.158:92/api/Attribute/AddAttributes", 
-  attributeUpdate: "http://10.48.30.158:92/api/Attribute/UpdateAttributes",
-  attributeGroup: "http://10.48.30.158:92/api/Attribute/GetAttributeGroup",
+//   attributes: "http://10.48.30.158:92/api/Attribute/GetAllAttributeID",
+//   attribute: "http://10.48.30.158:92/api/Attribute/GetAttributeDetails?attributeId=",
+//   attributeAdd: "http://10.48.30.158:92/api/Attribute/AddAttributes", 
+//   attributeUpdate: "http://10.48.30.158:92/api/Attribute/UpdateAttributes",
+//   attributeGroup: "http://10.48.30.158:92/api/Attribute/GetAttributeGroup",
 
-  benefits: "http://10.48.30.158:92/api/Benefits/GetAllBenefitsID",
-  benefit: "http://10.48.30.158:92/api/Benefits/GetBenefitsdetail/",
-  benefitAdd: "http://10.48.30.158:92/api/Benefits/AddBenefits",
-  benefitUpdate: "http://10.48.30.158:92/api/Benefits/UpdateBenefits",
+//   benefits: "http://10.48.30.158:92/api/Benefits/GetAllBenefitsID",
+//   benefit: "http://10.48.30.158:92/api/Benefits/GetBenefitsdetail/",
+//   benefitAdd: "http://10.48.30.158:92/api/Benefits/AddBenefits",
+//   benefitUpdate: "http://10.48.30.158:92/api/Benefits/UpdateBenefits",
 
-  programs: "http://10.48.30.158:92/api/Programs/GetAllProgramsID",
-  program: "http://10.48.30.158:92/api/Programs/GetProgramDetail/",
-  programAdd: "http://10.48.30.158:92/api/Programs/AddPrograms",
-  programUpdate: "http://10.48.30.158:92/api/Programs/UpdatePrograms",
+//   programs: "http://10.48.30.158:92/api/Programs/GetAllProgramsID",
+//   program: "http://10.48.30.158:92/api/Programs/GetProgramDetail/",
+//   programAdd: "http://10.48.30.158:92/api/Programs/AddPrograms",
+//   programUpdate: "http://10.48.30.158:92/api/Programs/UpdatePrograms",
 
-  contracts: "http://10.48.30.158:92/api/Contracts/GetAllContractsID/",
-  contractsByClientPath: "http://10.48.30.158:92/api/Contracts/GetAllContractsID/",
-  contract: "http://10.48.30.158:92/api/Contracts/GetContracts/",
-  contractAdd: "http://10.48.30.158:92/api/Contracts/AddContracts",
-  contractUpdate: "http://10.48.30.158:92/api/Contracts/UpdateContracts",
-  contractAll: "http://10.48.30.158:92/api/Contracts/GetAllContracts",
- contractDateValidation: "http://10.48.30.158:92/api/Contracts/DateValidation/",
+//   contracts: "http://10.48.30.158:92/api/Contracts/GetAllContractsID/",
+//   contractsByClientPath: "http://10.48.30.158:92/api/Contracts/GetAllContractsID/",
+//   contract: "http://10.48.30.158:92/api/Contracts/GetContracts/",
+//   contractAdd: "http://10.48.30.158:92/api/Contracts/AddContracts",
+//   contractUpdate: "http://10.48.30.158:92/api/Contracts/UpdateContracts",
+//   contractAll: "http://10.48.30.158:92/api/Contracts/GetAllContracts",
+//  contractDateValidation: "http://10.48.30.158:92/api/Contracts/DateValidation/",
 
-  clients: "http://10.48.30.158:92/api/Clients/LoadClientDetails",
-  client: "http://10.48.30.158:92/api/Clients/GetClientDetails/",
-  clientAdd: "http://10.48.30.158:92/api/Clients/AddClients",
-  clientUpdate: "http://10.48.30.158:92/api/Clients/UpdateClients",
-  getClientDetails: "http://10.48.30.158:92/api/Clients/GetClientDetails/",
-  activeClients:"http://10.48.30.158:92/api/Clients/GetActiveClients",
-  parentClient:"http://10.48.30.158:92/api/Clients/GetParentClient",
-    duplicateClientId: "http://10.48.30.158:92/api/Clients/DuplicateClientID?clientId=",
-    duplicateClientName: "http://10.48.30.158:92/api/Clients/DuplicateClient?client=",
+//   clients: "http://10.48.30.158:92/api/Clients/LoadClientDetails",
+//   client: "http://10.48.30.158:92/api/Clients/GetClientDetails/",
+//   clientAdd: "http://10.48.30.158:92/api/Clients/AddClients",
+//   clientUpdate: "http://10.48.30.158:92/api/Clients/UpdateClients",
+//   getClientDetails: "http://10.48.30.158:92/api/Clients/GetClientDetails/",
+//   activeClients:"http://10.48.30.158:92/api/Clients/GetActiveClients",
+//   parentClient:"http://10.48.30.158:92/api/Clients/GetParentClient",
+//     duplicateClientId: "http://10.48.30.158:92/api/Clients/DuplicateClientID?clientId=",
+//     duplicateClientName: "http://10.48.30.158:92/api/Clients/DuplicateClient?client=",
 
 
 
-  roles: "http://10.48.30.158:92/api/Roles/GetAllRoles",
-  role: "http://10.48.30.158:92/api/Roles/GetRolesDetail/",
-  roleAdd: "http://10.48.30.158:92/api/Roles/AddRoles",
-  roleUpdate: "http://10.48.30.158:92/api/Roles/UpdateRoles",
+//   roles: "http://10.48.30.158:92/api/Roles/GetAllRoles",
+//   role: "http://10.48.30.158:92/api/Roles/GetRolesDetail/",
+//   roleAdd: "http://10.48.30.158:92/api/Roles/AddRoles",
+//   roleUpdate: "http://10.48.30.158:92/api/Roles/UpdateRoles",
 
-  rules: "http://10.48.30.158:92/api/Rules/GetAllRuleID",
-  rule: "http://10.48.30.158:92/api/Rules/GetRulesDetails?ruleId=",
-  ruleGroups: "http://10.48.30.158:92/api/Rules/GetRuleGroup", 
-  ruleAdd: "http://10.48.30.158:92/api/Rules/AddRules",
-  ruleUpdate: "http://10.48.30.158:92/api/Rules/UpdateRules",
+//   rules: "http://10.48.30.158:92/api/Rules/GetAllRuleID",
+//   rule: "http://10.48.30.158:92/api/Rules/GetRulesDetails?ruleId=",
+//   ruleGroups: "http://10.48.30.158:92/api/Rules/GetRuleGroup", 
+//   ruleAdd: "http://10.48.30.158:92/api/Rules/AddRules",
+//   ruleUpdate: "http://10.48.30.158:92/api/Rules/UpdateRules",
   
-  claimCalculate: "http://10.48.30.158:92/api/Claim/CalculateClaimAmount/",
+//   claimCalculate: "http://10.48.30.158:92/api/Claim/CalculateClaimAmount/",
 
-  UsersPath:"http://10.48.30.158:92/api/UserAdministration/GetAllUsers",
-  UserDetailPath:"http://10.48.30.158:92/api/UserAdministration/GetUserDetails/",
-  userAdd:"http://10.48.30.158:92/api/UserAdministration/AddUsers",
-  userTerminate:"http://10.48.30.158:92/api/UserAdministration/TerminateUsers",
-  userChange:"http://10.48.30.158:92/api/UserAdministration/ChangeUsers",
+//   UsersPath:"http://10.48.30.158:92/api/UserAdministration/GetAllUsers",
+//   UserDetailPath:"http://10.48.30.158:92/api/UserAdministration/GetUserDetails/",
+//   userAdd:"http://10.48.30.158:92/api/UserAdministration/AddUsers",
+//   userTerminate:"http://10.48.30.158:92/api/UserAdministration/TerminateUsers",
+//   userChange:"http://10.48.30.158:92/api/UserAdministration/ChangeUsers",
  
-  BatchStatusPath:"http://10.48.30.158:92/api/BatchProcess/GetBatchStatus",
-  BatchProcessPath:"http://10.48.30.158:92/api/BatchProcess/GetBatchProcessDetail/",
-  BatchHistoryPath:"http://10.48.30.158:92/api/BatchProcess/GetBatchProcessHistoryDetail?batchProcessId=",
-  BatchProcessAdd: "http://10.48.30.158:92/api/BatchProcess/AddBatchProcess",
-  BatchProcessUpdate: "http://10.48.30.158:92/api/BatchProcess/UpdateBatchProcess",
-  BatchProcessCalculate: "http://10.48.30.158:92/api/BatchProcess/BatchCalculateClaimAmount?",
+//   BatchStatusPath:"http://10.48.30.158:92/api/BatchProcess/GetBatchStatus",
+//   BatchProcessPath:"http://10.48.30.158:92/api/BatchProcess/GetBatchProcessDetail/",
+//   BatchHistoryPath:"http://10.48.30.158:92/api/BatchProcess/GetBatchProcessHistoryDetail?batchProcessId=",
+//   BatchProcessAdd: "http://10.48.30.158:92/api/BatchProcess/AddBatchProcess",
+//   BatchProcessUpdate: "http://10.48.30.158:92/api/BatchProcess/UpdateBatchProcess",
+//   BatchProcessCalculate: "http://10.48.30.158:92/api/BatchProcess/BatchCalculateClaimAmount?",
 
-  planTires: "http://10.48.30.158:92/api/Plan/GetTiers",
-  planAll: "http://10.48.30.158:92/api/Plan/GetPlanDetails",
-  planAdd: "http://10.48.30.158:92/api/Plan/AddPlan",
-  planUpdate: "http://10.48.30.158:92/api/Plan/UpdatePlan",
+//   planTires: "http://10.48.30.158:92/api/Plan/GetTiers",
+//   planAll: "http://10.48.30.158:92/api/Plan/GetPlanDetails",
+//   planAdd: "http://10.48.30.158:92/api/Plan/AddPlan",
+//   planUpdate: "http://10.48.30.158:92/api/Plan/UpdatePlan",
 
-  memberSearch: "http://10.48.30.158:92/api/Member/GetMemberdetail?",
-  memberAdd: "http://10.48.30.158:92/api/Member/AddMember",
-  memberUpdate: "http://10.48.30.158:92/api/Member/UpdateMember",
-    memberAll: "http://10.48.30.158:92/api/Member/GetAllMember",
-    member:"http://10.48.30.158:92/api/Member/GetMemberDetailsByClientId/",
+//   memberSearch: "http://10.48.30.158:92/api/Member/GetMemberdetail?",
+//   memberAdd: "http://10.48.30.158:92/api/Member/AddMember",
+//   memberUpdate: "http://10.48.30.158:92/api/Member/UpdateMember",
+//     memberAll: "http://10.48.30.158:92/api/Member/GetAllMember",
+//     member:"http://10.48.30.158:92/api/Member/GetMemberDetailsByClientId/",
   
 
-  productAll: "http://10.48.30.158:92/api/Product/GetAllProducts",
-  product: "http://10.48.30.158:92/api/Product/GetProducts?productId=",
-  productAdd: "http://10.48.30.158:92/api/Product/AddProducts",
-  productUpdate:"http://10.48.30.158:92/api/Product/UpdateProduct",
-  productCovertClaims:"http://10.48.30.158:92/api/Product/GetCoveredClaims",
+//   productAll: "http://10.48.30.158:92/api/Product/GetAllProducts",
+//   product: "http://10.48.30.158:92/api/Product/GetProducts?productId=",
+//   productAdd: "http://10.48.30.158:92/api/Product/AddProducts",
+//   productUpdate:"http://10.48.30.158:92/api/Product/UpdateProduct",
+//   productCovertClaims:"http://10.48.30.158:92/api/Product/GetCoveredClaims",
 
- ExcelUploadPath: "http://10.48.30.158:92/api/ExcelUpload/UploadExcel?userId=",
+//  ExcelUploadPath: "http://10.48.30.158:92/api/ExcelUpload/UploadExcel?userId=",
+  
+//   sslReportURL: "http://dwim4-obiee01.qdint.local:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FPOC%2FAgg_Claim_Expenese",
+//   aslReportURL: "http://dwim4-obiee01.qdint.local:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FPOC%2FAgg_Claim_Expenese",
+//   maxLiabilityReportURL: "http://dwim4-obiee01.qdint.local:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FPOC%2FAgg_Claim_Expenese"
+
+// }
+
+
+export const Paths = {
+  loginPath : "http://localhost:92/api/Login/UserLogin/",
+  reportPath : "http://localhost:92/api/Claim/GetClaimReport",
+  claimPath: "http://localhost:92/api/Claim/GetClaim",
+  
+  
+  attributes: "http://localhost:92/api/Attribute/GetAllAttributeID",
+  attribute: "http://localhost:92/api/Attribute/GetAttributeDetails?attributeId=",
+  attributeAdd: "http://localhost:92/api/Attribute/AddAttributes", 
+  attributeUpdate: "http://localhost:92/api/Attribute/UpdateAttributes",
+  attributeGroup: "http://localhost:92/api/Attribute/GetAttributeGroup",
+
+  benefits: "http://localhost:92/api/Benefits/GetAllBenefitsID",
+  benefit: "http://localhost:92/api/Benefits/GetBenefitsdetail/",
+  benefitAdd: "http://localhost:92/api/Benefits/AddBenefits",
+  benefitUpdate: "http://localhost:92/api/Benefits/UpdateBenefits",
+
+  programs: "http://localhost:92/api/Programs/GetAllProgramsID",
+  program: "http://localhost:92/api/Programs/GetProgramDetail/",
+  programAdd: "http://localhost:92/api/Programs/AddPrograms",
+  programUpdate: "http://localhost:92/api/Programs/UpdatePrograms",
+
+  contracts: "http://localhost:92/api/Contracts/GetAllContractsID/",
+  contractsByClientPath: "http://localhost:92/api/Contracts/GetAllContractsID/",
+  contract: "http://localhost:92/api/Contracts/GetContracts/",
+  contractAdd: "http://localhost:92/api/Contracts/AddContracts",
+  contractUpdate: "http://localhost:92/api/Contracts/UpdateContracts",
+  contractAll: "http://localhost:92/api/Contracts/GetAllContracts",
+ contractDateValidation: "http://localhost:92/api/Contracts/DateValidation/",
+
+  clients: "http://localhost:92/api/Clients/LoadClientDetails",
+  client: "http://localhost:92/api/Clients/GetClientDetails/",
+  clientAdd: "http://localhost:92/api/Clients/AddClients",
+  clientUpdate: "http://localhost:92/api/Clients/UpdateClients",
+  getClientDetails: "http://localhost:92/api/Clients/GetClientDetails/",
+  activeClients:"http://localhost:92/api/Clients/GetActiveClients",
+  parentClient:"http://localhost:92/api/Clients/GetParentClient",
+    duplicateClientId: "http://localhost:92/api/Clients/DuplicateClientID?clientId=",
+    duplicateClientName: "http://localhost:92/api/Clients/DuplicateClient?client=",
+
+
+
+  roles: "http://localhost:92/api/Roles/GetAllRoles",
+  role: "http://localhost:92/api/Roles/GetRolesDetail/",
+  roleAdd: "http://localhost:92/api/Roles/AddRoles",
+  roleUpdate: "http://localhost:92/api/Roles/UpdateRoles",
+
+  rules: "http://localhost:92/api/Rules/GetAllRuleID",
+  rule: "http://localhost:92/api/Rules/GetRulesDetails?ruleId=",
+  ruleGroups: "http://localhost:92/api/Rules/GetRuleGroup", 
+  ruleAdd: "http://localhost:92/api/Rules/AddRules",
+  ruleUpdate: "http://localhost:92/api/Rules/UpdateRules",
+  
+  claimCalculate: "http://localhost:92/api/Claim/CalculateClaimAmount/",
+
+  UsersPath:"http://localhost:92/api/UserAdministration/GetAllUsers",
+  UserDetailPath:"http://localhost:92/api/UserAdministration/GetUserDetails/",
+  userAdd:"http://localhost:92/api/UserAdministration/AddUsers",
+  userTerminate:"http://localhost:92/api/UserAdministration/TerminateUsers",
+  userChange:"http://localhost:92/api/UserAdministration/ChangeUsers",
+ 
+  BatchStatusPath:"http://localhost:92/api/BatchProcess/GetBatchStatus",
+  BatchProcessPath:"http://localhost:92/api/BatchProcess/GetBatchProcessDetail/",
+  BatchHistoryPath:"http://localhost:92/api/BatchProcess/GetBatchProcessHistoryDetail?batchProcessId=",
+  BatchProcessAdd: "http://localhost:92/api/BatchProcess/AddBatchProcess",
+  BatchProcessUpdate: "http://localhost:92/api/BatchProcess/UpdateBatchProcess",
+  BatchProcessCalculate: "http://localhost:92/api/BatchProcess/BatchCalculateClaimAmount?",
+
+  planTires: "http://localhost:92/api/Plan/GetTiers",
+  planAll: "http://localhost:92/api/Plan/GetPlanDetails",
+  planAdd: "http://localhost:92/api/Plan/AddPlan",
+  planUpdate: "http://localhost:92/api/Plan/UpdatePlan",
+
+  memberSearch: "http://localhost:92/api/Member/GetMemberdetail?",
+  memberAdd: "http://localhost:92/api/Member/AddMember",
+  memberUpdate: "http://localhost:92/api/Member/UpdateMember",
+    memberAll: "http://localhost:92/api/Member/GetAllMember",
+    member:"http://localhost:92/api/Member/GetMemberDetailsByClientId/",
+  
+
+  productAll: "http://localhost:92/api/Product/GetAllProducts",
+  product: "http://localhost:92/api/Product/GetProducts?productId=",
+  productAdd: "http://localhost:92/api/Product/AddProducts",
+  productUpdate:"http://localhost:92/api/Product/UpdateProduct",
+  productCovertClaims:"http://localhost:92/api/Product/GetCoveredClaims",
+
+ ExcelUploadPath: "http://localhost:92/api/ExcelUpload/UploadExcel?userId=",
   
   sslReportURL: "http://dwim4-obiee01.qdint.local:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FPOC%2FAgg_Claim_Expenese",
   aslReportURL: "http://dwim4-obiee01.qdint.local:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FPOC%2FAgg_Claim_Expenese",
   maxLiabilityReportURL: "http://dwim4-obiee01.qdint.local:9502/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FPOC%2FAgg_Claim_Expenese"
 
 }
+
+
